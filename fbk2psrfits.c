88c06d664763f84d73895ec3327c2223c35c7804
Gregory Desvignes <gdesvignes.astro@gmail.com>
2019-01-30 10:27:26 +0100

    Can now convert 32bit filterbank format. Also option to specify specific receivers for Effelsberg

--- fbk2psrfits.c
+++ fbk2psrfits.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdbool.h>
 #include "psrfits.h"
 #include "fbk_header.h"
 
@@ -11,6 +14,20 @@
 #ifndef RADTODEG
 #define RADTODEG 57.29577951308232087679815481410517033240547246656
 #endif
+
+
+void usage() {
+  printf(
+		             "Usage: fbk2psrfits [options] input_filename_base or filenames\n"
+					 "Options:\n"
+					 "  -h, --help               Print this\n"
+					 "  -F, --frontend=FRONTEND  Frontend name, e.g. S36, S60, S45, S110\n"
+					 "  -o, --off_chan           Correct the frequency by half a chan width\n"
+					 "  -s, --srcname=NAME       Update source name\n "
+					 "  -S HAve Stokes\n"
+		 );
+}
+					 
 
 void dec2hms(char *out, double in, int sflag) {
     int sign = 1;
@@ -27,20 +44,58 @@ void dec2hms(char *out, double in, int sflag) {
 }
 
 int main(int argc, char *argv[]) {
+
+  /* Cmd line */
+  static struct option long_opts[] = {
+	{"frontend",   1, NULL, 'F'},
+	{"offset",   0, NULL, 'o'},
+	{"srcname",   1, NULL, 's'},
+  };
+  
     int ii;
     double dtmp;
     char filename[256], *adr;
     struct psrfits pf;
     int c1,c2;
     double c3;
+	int opt, opti;
     int scan_id, beam_id;
+	bool have_Stokes = false;
+	bool have_chan_offset = false;
+	bool have_frontend = false; char frontend[3];
+	bool have_srcname = false; char srcname[24];
+
+	while ((opt=getopt_long(argc,argv,"ob:t:j:i:F:s:p:P:F:CuU:SAqh",long_opts,&opti))!=-1) {
+	  switch (opt) {
+	  case 'F':
+		have_frontend = true;
+		strncpy(frontend, optarg, 3);
+		break;
+	  case 'o':
+		have_chan_offset = true;
+		break;
+	  case 's':
+		have_srcname = true;
+		strncpy(srcname, optarg, 24);
+		break;
+	  case 'S':
+		have_Stokes = true;
+		break;
+	  case 'h':
+	  default:
+		usage();
+		exit(0);
+		break;
+	  }
+
+	}
     
     // Only set the basefilename and not "filename"
     // Also, fptr will be set by psrfits_create_searchmode()
 
     FILE *pfi;
 
-    strcpy(filename, argv[1]);
+    strcpy(filename, argv[optind]);
     printf("Filename = %s\n", filename);
 
     // Read filterbank                                                                              
@@ -63,7 +118,7 @@ int main(int argc, char *argv[]) {
     // Get the number of samples                                                                              
     numsamps=nsamples(filename, header_size, &iheader);
     //printf("numsamps = %lld  ra=%lf  dec=%lf\n", numsamps, iheader.src_raj, iheader.src_dej);
-    fflush(stdout);
+    //fflush(stdout);
     
     pf.filenum = 0;           // This is the crucial one to set to initialize things
     pf.rows_per_file = 4000;  // Need to set this based on PSRFITS_MAXFILELEN
@@ -91,6 +146,9 @@ int main(int argc, char *argv[]) {
       
     pf.hdr.dt = iheader.tsamp;
     pf.hdr.fctr = iheader.fch1+iheader.foff*iheader.nchans/2. - iheader.foff/2.;
+	if (have_chan_offset)
+	  pf.hdr.fctr += iheader.foff/2.;
+	
     pf.hdr.BW = iheader.nchans * iheader.foff;
     angle_split(iheader.src_raj, &c1, &c2, &c3);
     sprintf(pf.hdr.ra_str, "%2.2d:%2.2d:%07.4lf", c1, c2, c3);
@@ -105,7 +163,6 @@ int main(int argc, char *argv[]) {
     pf.hdr.start_day = 55000;
     pf.hdr.scan_number = scan_id;
     pf.hdr.rcvr_polns = 2;
-    pf.hdr.onlyI = 1;
     pf.hdr.summed_polns = 0;
     pf.hdr.offset_subint = 0;
     pf.hdr.nchan = iheader.nchans;
@@ -113,22 +170,22 @@ int main(int argc, char *argv[]) {
     pf.hdr.orig_df = pf.hdr.df = pf.hdr.BW / pf.hdr.nchan;
     pf.hdr.nbits = iheader.nbits;
     pf.hdr.npol = iheader.nifs;
+	pf.hdr.onlyI = 0;
+	if (pf.hdr.npol==1) pf.hdr.onlyI = 1;
     pf.hdr.chan_dm = 0.0;
     pf.hdr.fd_hand = 1;
     pf.hdr.fd_sang = 0;
     pf.hdr.fd_xyph = 0;
     pf.hdr.be_phase = 1;
     pf.hdr.ibeam = beam_id;
-    fflush(stdout);
     pf.hdr.nsblk = 2048;
 
     pf.hdr.MJD_epoch = iheader.tstart;  // Note the "L" for long double
     pf.hdr.ds_time_fact = 1;
     pf.hdr.ds_freq_fact = 1;
-    sprintf(pf.basefilename, "%s_%04d_%02d", pf.hdr.backend, pf.hdr.scan_number, pf.hdr.ibeam);
-    fflush(stdout);
+    //sprintf(pf.basefilename, "%s_%04d_%02d", pf.hdr.backend, pf.hdr.scan_number, pf.hdr.ibeam);
 
-    psrfits_create(&pf);
+    //psrfits_create(&pf);
 
 
     // Now set values for our subint structure
@@ -149,8 +206,48 @@ int main(int argc, char *argv[]) {
     pf.sub.tel_zen = pf.hdr.zenith_ang;
     pf.sub.bytes_per_subint = (pf.hdr.nbits * pf.hdr.nchan * 
                                pf.hdr.npol * pf.hdr.nsblk) / 8;
-    pf.sub.FITS_typecode = TBYTE;  // 11 = byte
+	
+    if (pf.hdr.nbits==32) pf.sub.FITS_typecode = TFLOAT;
+	else pf.sub.FITS_typecode = TBYTE;  // 11 = byte
 
+	//
+	if (have_frontend) {
+	  if (strncmp(frontend,"S36",3)==0) {
+		strcpy(pf.hdr.backend, "PSRIX");
+		strcpy(pf.hdr.frontend, "S36");
+		strcpy(pf.hdr.poln_type, "CIRC");
+		strcpy(pf.hdr.poln_order, "AABBCRCI");
+	  }
+	  else if (strncmp(frontend,"S60",3)==0) {
+		strcpy(pf.hdr.backend, "PSRIX");
+		strcpy(pf.hdr.frontend, "S60");
+		strcpy(pf.hdr.poln_type, "CIRC");
+		strcpy(pf.hdr.poln_order, "AABBCRCI");		
+	  }
+	  else if (strncmp(frontend,"S110",3)==0) {
+		strcpy(pf.hdr.backend, "PSRIX");
+		strcpy(pf.hdr.frontend, "S110");
+		strcpy(pf.hdr.poln_type, "CIRC");
+		strcpy(pf.hdr.poln_order, "AABBCRCI");
+	  }
+	  else if (strncmp(frontend,"S45",3)==0) {
+		strcpy(pf.hdr.backend, "PSRIX");
+		strcpy(pf.hdr.frontend, "S45");
+		strcpy(pf.hdr.poln_type, "LIN");
+		strcpy(pf.hdr.poln_order, "AABBCRCI");
+	  }
+	  else {printf("Frontend %s not recognised\n", frontend);}
+	}
+	if (have_srcname) strncpy(pf.hdr.source, srcname, 24);
+
+	if (have_Stokes) strcpy(pf.hdr.poln_order, "IQUV");
+	
+	if (strncmp(pf.hdr.backend, "PFFTS",5)==0)
+	  sprintf(pf.basefilename, "%s_%04d_%02d", pf.hdr.backend, pf.hdr.scan_number, pf.hdr.ibeam);
+	else
+	  sprintf(pf.basefilename, "%s_%s_%5d_%d", pf.hdr.backend, pf.hdr.source, (int) pf.hdr.MJD_epoch, (int) pf.hdr.fctr);
+	psrfits_create(&pf);
+	
     // Create and initialize the subint arrays
     pf.sub.dat_freqs = (float *)malloc(sizeof(float) * pf.hdr.nchan);
     pf.sub.dat_weights = (float *)malloc(sizeof(float) * pf.hdr.nchan);
